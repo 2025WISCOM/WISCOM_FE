c314e91bc48f958b706e1c9731a102b7d1c88995
jinhyo <kjhyo0220@gmail.com>
2025-08-30 02:58:32 +0900

    ✨ [Feat] stamp 페이지 헤더 구현

---
+++ src/components/stamp/BoothStamp.tsx
@@ -0,0 +1,5 @@
+const BoothStamp = () => {
+  return <div>부스 스탬프</div>
+}
+
+export default BoothStamp
---
+++ src/components/stamp/FloorPlan.tsx
@@ -0,0 +1,5 @@
+const FloorPlan = () => {
+  return <div>부스 배치도</div>
+}
+
+export default FloorPlan
