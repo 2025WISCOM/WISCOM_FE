a0991bdab32e4bd326d854cef85a1ab0e50650ac
jinhyo <kjhyo0220@gmail.com>
2025-08-30 05:21:56 +0900

    ✨ [Feat] 스탬프 페이지 구현

--- src/components/stamp/BoothStamp.tsx
+++ src/components/stamp/BoothStamp.tsx
@@ -1,5 +1,44 @@
+import StampImg from '../../assets/duksung_fantasy_transparent.png'
+import ComputerIcon from '../../assets/booth_stamp_computer.svg'
+import SuccessIcon from '../../assets/booth_stamp_success.svg'
+
 const BoothStamp = () => {
-  return <div>부스 스탬프</div>
+  return (
+    <>
+      <div className="pt-[20px] px-[20px]">
+        <h1 className="text-[#8F7860] text-[22px] font-medium leading-[32px]">
+          Event
+          <span className="ml-[8px] text-[#8F7860] text-[15px] font-medium leading-[24px]">
+            부스를 돌며 스탬프를 모아보세요!
+          </span>
+        </h1>
+      </div>
+
+      <div className="px-[20px] mt-[14px]">
+        <img
+          src={StampImg}
+          alt="부스 스탬프"
+          className="w-full h-full object-contain"
+        />
+      </div>
+
+      <div className="grid grid-cols-3 gap-[24px] px-[35px] place-items-center mb-[36px]">
+        {Array.from({ length: 15 }).map((_, idx) => (
+          <div className="relative w-[80px] h-[80px] flex items-center justify-center  cursor-pointer">
+            <img
+              key={idx}
+              src={ComputerIcon}
+              alt="컴퓨터 아이콘"
+              className="w-full h-full object-contain absolute inset-0"
+            />
+            <p className="font-['Butler'] text-[#9D8469] text-[22px] font-medium leading-[32px] relative top-[-4px]">
+              {idx + 1}
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default BoothStamp
