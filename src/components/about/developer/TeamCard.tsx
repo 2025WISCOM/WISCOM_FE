05bf6672d39b68952758df9f4ae556b9b47d6d88
phjlia2430 <phjlia2430@naver.com>
2025-10-21 00:18:21 +0900

    ✨ [Feat] 1차 qa about

--- src/components/about/developer/TeamCard.tsx
+++ src/components/about/developer/TeamCard.tsx
@@ -6,14 +6,18 @@ interface TeamCardProps {
 
 const TeamCard = ({ image, teamName, members }: TeamCardProps) => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center text-center w-[150px] h-[250px]">
       <img
         src={image}
         alt={teamName}
-        className="w-[100px] h-[100px] object-contain"
+        className="w-[100px] h-[100px] object-contain mb-2"
       />
-      <p className="mt-2 text-sm font-semibold text-[#6F5E4B]">{teamName}</p>
-      <ul className="mt-2 space-y-1 text-sm text-black text-center">
+
+      <p className="text-sm font-semibold text-[#6F5E4B] h-[22px] flex items-center justify-center">
+        {teamName}
+      </p>
+
+      <ul className="mt-2 space-y-1 text-sm text-black leading-[18px]">
         {members.map((m, idx) => (
           <li key={idx}>{m}</li>
         ))}
