7cd3f8951c64baced532f05db6998ea01c012f9c
heeone1 <rinakim1019@gmail.com>
2025-10-24 18:22:00 +0900

    🐛 [FIX] 로딩 처리 404 페이지 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -45,7 +45,7 @@ const MainPage = () => {
     const to = setTimeout(() => {
       sessionStorage.removeItem('fromSplash')
       navigate('/about/introduction', { replace: true })
-    }, 4500)
+    }, 5000)
     return () => clearTimeout(to)
   }, [fromSplash, navigate])
 
